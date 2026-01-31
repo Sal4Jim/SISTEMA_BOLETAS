4ce673fb933ea2e53be34238157cb2e7d320b139
Jimmy Montoya <jimmymontoyam@gmail.com>
2026-01-31 14:34:26 -0500

    V1.0

--- MYPK2/app/src/main/java/com/mypk2/app/api/RetrofitClient.java
+++ MYPK2/app/src/main/java/com/mypk2/app/api/RetrofitClient.java
@@ -14,16 +14,8 @@ public class RetrofitClient {
     // private static final String BASE_URL = "http://10.0.2.2:3000/api/mobile/";
 
     // Para dispositivo físico con tu IP
-    // Para dispositivo físico con tu IP
-    public static final String SERVER_URL = "http://192.168.0.200:3000";
+    public static final String SERVER_URL = "http://[IP_ADDRESS]";
     public static final String BASE_URL = SERVER_URL + "/api/mobile/";
-
-    // O puedes usar esta configuración dinámica:
-    // public static final boolean USE_EMULATOR = false; // true para emulador,
-    // false para físico
-    // private static final String BASE_URL = USE_EMULATOR ?
-    // "http://10.0.2.2:3000/api/mobile/" :
-    // "http://192.168.1.105:3000/api/mobile/";
 
     private static Retrofit retrofit = null;
     private static ApiService apiService = null;
